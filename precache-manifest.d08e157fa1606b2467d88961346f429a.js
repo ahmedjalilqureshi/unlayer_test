af0cb29a256ec2002dcd14bd269304e6c54c9bde
ahmedjalilqureshi <rapshek@gmail.com>
2019-10-27 16:51:33 +0500

    Updates

---
+++ precache-manifest.d08e157fa1606b2467d88961346f429a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd29b029d07716285132bdcd6b2be8b4",
+    "url": "/unlayer_test/index.html"
+  },
+  {
+    "revision": "38111162a442f4d0147d",
+    "url": "/unlayer_test/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "09652e4aa68d19bda6a3",
+    "url": "/unlayer_test/static/js/2.7615dea0.chunk.js"
+  },
+  {
+    "revision": "38111162a442f4d0147d",
+    "url": "/unlayer_test/static/js/main.9096b0fa.chunk.js"
+  },
+  {
+    "revision": "1b0335ee875629772954",
+    "url": "/unlayer_test/static/js/runtime-main.dca55398.js"
+  }
+]);
